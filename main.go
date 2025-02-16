7b6dd7722a0755e12703a1c33a3d089cca0078b5
shin1244 <tlswnsrb1357@gmail.com>
2025-02-16 22:57:56 +0900

    http 핸들러와 웹소켓에서는 이미 동시성 프로그래밍을 지원하고 있었음음

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"web/app"
+	"sync"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/pat"
 )
 
+type Server struct {
+	RoomList []*Room `json:"room_list"`
+	Mutex    sync.RWMutex
+}
+
+type Room struct {
+	ID string `json:"id"`
+	// Clients  []*Client  `json:"clients"`
+	// Messages []*Message `json:"messages"`
+}
+
 func main() {
 	mux := pat.New()
-	server := app.NewServer()
 
-	mux.Post("/rooms", server.CreateRoom)
+	server := NewServer()
+
 	mux.Get("/rooms", server.GetRooms)
 
 	n := negroni.Classic()
 	n.UseHandler(mux)
 
 	http.ListenAndServe(":8080", n)
+}
+
+func NewServer() *Server {
+	return &Server{
+		RoomList: make([]*Room, 0),
+	}
+}
+
+func (s *Server) GetRooms(w http.ResponseWriter, r *http.Request) {
+	s.Mutex.RLock()
+	defer s.Mutex.RUnlock()
+
+	json.NewEncoder(w).Encode(s.RoomList)
 }
